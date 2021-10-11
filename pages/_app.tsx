62e3c262948b129a0b9020746bb8e3d04fdf639f
José Márquez Doblas <jose.marquez@atlantyasoftware.com>
2021-10-11 17:21:17 +0200

    Homepage style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider as NextAuthProvider } from 'next-auth/client';
+import { ChakraProvider } from "@chakra-ui/react"
 
 
 // Use the <Provider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App ({ Component, pageProps }) {
   return (
-    <NextAuthProvider session={pageProps.session} >
-      <Component {...pageProps} />
-    </NextAuthProvider>
+    <ChakraProvider>
+      <NextAuthProvider session={pageProps.session} >
+        <Component {...pageProps} />
+      </NextAuthProvider>
+    </ChakraProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import Link from 'next/link';
+import { 
+  Image,
+  Center,
+  Button,
+  Box,
+  Heading
+} from '@chakra-ui/react';
 
 export default function Home() {
 
   return (
-    <div>
-      <span>Hola Mundo</span><br/>
-      <Link href="/signup">
-          <a>Sign up</a>
-      </Link><br/>
-      <Link href="/login">
-          <a>Login</a>
-      </Link><br/>
-      <Link href="/brand/create">
-          <a>Crear Marca</a>
-      </Link><br/>
-      <Link href="/brand/">
-          <a>Marcas</a>
-      </Link>
-    </div>
+    <Center pt="100">
+      <Box maxW="sm" overflow="hidden">
+        <Center pt="50">
+          <Image src="/logo.png" alt="Plato logo" />
+        </Center>
+        <Box p="6">
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+            textAlign="center"
+          >
+            <Heading size="md">
+              Plato: Controla tus redes
+            </Heading>
+          </Box>
+            <Center pt="5">
+              <Link href="/signup">
+                <Button colorScheme="teal" mr="4">
+                  Registrarme
+                </Button>
+              </Link>
+              <Link href="/login">
+                <Button colorScheme="teal">
+                  Iniciar sesión
+                </Button>
+              </Link>
+            </Center>
+        </Box>
+      </Box>
+    </Center>
   );
 }
 
